4f90ca1d298f858b431f578be71c827057566bce
HasbersHz <98599824+HasbersHz@users.noreply.github.com>
2023-09-15 17:52:28 +0000

    Add stack

--- lexer.c
+++ lexer.c
@@ -9,3 +9,36 @@ struct Token {
     // str self.type + "<" + repr(self.value) + " at " + str(self.pos) + ">"    
     // repr "Token(" + repr(self.type) + ", " + repr(self.value) + ", " + "Pos(" + repr(self.pos.start) + ", " + repr(self.pos.end) + "))"
 }
+
+// #define STACK_SIZE 1000
+
+typedef struct Stack { 
+    char* data
+    struct Stack * next;
+} stack;
+
+stack new_stack() {
+    stack S;
+    S.data = NULL;
+    S.next = NULL;
+    return S;
+}
+
+char is_emptyCh(stack* S) {
+    return !(S->data == 0);
+}
+
+void pushCh(stack )
+{
+    
+}
+
+void popCh()
+{
+    
+}
+
+void showCh()
+{
+    
+}
