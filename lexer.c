9c5516f2a60e6bd3488048d7d9845576eef7ea3d
HasbersHz <98599824+HasbersHz@users.noreply.github.com>
2023-09-17 18:12:11 +0000

    bad moving lexer from python

--- lexer.c
+++ lexer.c
@@ -52,3 +52,178 @@ char* popCh(stack* S) {
 char* readCh(stack* S) {
     return S->data;
 }
+
+def lex(self) {
+    code = self.code
+
+    int i;
+
+    for (i = 0;i < strlen();i++) {
+        if code[i] in ALPHA:
+            temp = code[i]
+            i++;
+            while code[i] in ALNUM and len(temp) < 63:
+                temp += code[i]
+                i++;
+            else:
+                i--;
+                self.tokens.append(Token(NAME, temp, Pos(i-len(temp), i)))
+        
+        elif code[i] in DIGIT and (code[i] != "0" or code[i+1] not in DIGIT):
+            temp = code[i]
+            i+=1
+            while code[i] in DIGIT and len(temp) < 20:
+                temp += code[i]
+                i+=1
+            else:
+                i-=1
+                self.tokens.append(Token(INT, temp, Pos(i-len(temp), i)))
+        
+        elif code[i] == '"':
+            i+=1
+            temp = ""
+            while code[i] != '"':
+                if code[i] == "\\":
+                    if code[i+1] == '"':
+                        i+=1
+                        temp += '"'
+                    else:
+                        temp += "\\"
+                else:
+                    temp += code[i]
+                i+=1
+            self.tokens.append(Token(STRING, eval('"'+temp+'"'), Pos(i-len(temp), i)))
+        
+        elif code[i] == ",":
+            self.tokens.append(Token(COMMA, ",", Pos(i, i)))
+        
+        elif code[i] == "<":
+            if code[i+1] == "-":
+                self.tokens.append(Token(ARL, "<-", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(LE, "<=", Pos(i, i:=i+1)))
+            elif code[i+1] == "<":
+                self.tokens.append(Token(SHL, "<<", Pos(i, i:=i+1)))
+            elif code[i+1] == "~":
+                self.tokens.append(Token(RESERVED, "<-", Pos(i, i:=i+1)))
+            elif code[i+1] == ">":
+                self.tokens.append(Token(ARRAYTYPE, "<-", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(LT, "<", Pos(i, i)))
+        
+        elif code[i] == ">":
+            if code[i+1] == "=":
+                self.tokens.append(Token(GE, ">=", Pos(i, i:=i+1)))
+            elif code[i+1] == ">":
+                self.tokens.append(Token(SHR, ">>", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(GT, ">", Pos(i, i)))
+        
+        elif code[i] == "=":
+            if code[i+1] == ">":
+                self.tokens.append(Token(SO, "=>", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(EQUAL, "==", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(ASSIGN, "=", Pos(i, i)))
+        
+        elif code[i] == ".":
+            if code[i+1] == ".":
+                self.tokens.append(Token(RANGE, "..", Pos(i, i:=i+1)))
+            elif code[i+1]+code[i+2] == "..":
+                self.tokens.append(Token(ELLIPSE, "...", Pos(i, i:=i+2)))
+            else:
+                self.tokens.append(Token(DOT, ".", Pos(i, i)))
+        
+        elif code[i] == "!":
+            if code[i+1] == "=":
+                self.tokens.append(Token(NE, "!=", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(NOT, "!", Pos(i, i)))
+        
+        elif code[i] == ":":
+            self.tokens.append(Token(COLON, ":", Pos(i, i)))
+        
+        elif code[i] == "-":
+            if code[i+1] == ">":
+                self.tokens.append(Token(ARR, "->", Pos(i, i:=i+1)))
+            if code[i+1] == "=":
+                self.tokens.append(Token(MINUSASSIGN, "-=", Pos(i, i:=i+1)))
+            if code[i+1] == "-":
+                self.tokens.append(DEC, "--", Pos(i, i:=i+1))
+            else:
+                self.tokens.append(Token(MINUS, "-", Pos(i, i)))
+        
+        elif code[i] == "+":
+            if code[i+1] == "=":
+                self.tokens.append(Token(PLUSASSIGN, "+=", Pos(i, i:=i+1)))
+            if code[i+1] == "-":
+                self.tokens.append(INC, "++", Pos(i, i:=i+1))
+            else:
+                self.tokens.append(Token(PLUS, "+", Pos(i, i)))
+        
+        elif code[i] == "~":
+            if code[i+1] == "~":
+                self.tokens.append(Token(NEGASSIGN, "~~", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(NEG, "~", Pos(i, i)))
+        
+        elif code[i] == "$":
+            self.tokens.append(Token(SYSTAG, "$", Pos(i, i)))
+        
+        elif code[i] == "*":
+            if code[i+1] == "*":
+                if code[i+2] == "=":
+                    self.tokens.append(Token(POWASSIGN, "**=", Pos(i, i:=i+2)))
+                else:
+                    self.tokens.append(Token(POW, "**", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(MULASSIGN, "*=", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(MUL, "*", Pos(i, i)))
+        
+        elif code[i] == "/":
+            if code[i+1] == "/":
+                if code[i+2] == "=":
+                    self.tokens.append(Token(IDIVASSIGN, "//=", Pos(i, i:=i+2)))
+                else:
+                    self.tokens.append(Token(IDIV, "//", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(DIVASSIGN, "/=", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(DIV, "/", Pos(i, i)))
+        
+        elif code[i] == "%":
+            if code[i+1] == "%":
+                self.tokens.append(Token(MODAPPLY, f"%%", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(MODASSIGN, "%=", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(MOD, "%", Pos(i, i)))
+        
+        elif code[i] == "|":
+            if code[i+1] == "|":
+                self.tokens.append(Token(BOOLOR, "||", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(ORASSIGN, "|=", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(OR, "|", Pos(i, i)))
+        
+        elif code[i] == "^":
+            if code[i+1] == "^":
+                self.tokens.append(Token(BOOLXOR, "^^", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(XORASSIGN, "^=", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(XOR, "^", Pos(i, i)))
+        
+        elif code[i] == "&":
+            if code[i+1] == "&":
+                self.tokens.append(Token(BOOLAND, "&&", Pos(i, i:=i+1)))
+            elif code[i+1] == "=":
+                self.tokens.append(Token(ANDASSIGN, "&=", Pos(i, i:=i+1)))
+            else:
+                self.tokens.append(Token(AND, "&", Pos(i, i)))
+        
+        elif code[i] in "()[]{}":
+            self.tokens.append(Token("PAL PAR BLL BLR CUL CUR".split()["()[]{}".index(code[i])], code[i], Pos(i, i)))
